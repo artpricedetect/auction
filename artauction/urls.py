9f73f98fdaa992f6bdc9f076f48ddc934a75379f
Youngmin <jeonggon8709@gmail.com>
2022-02-03 13:12:47 +0900

    refac: apply lint

--- artauction/urls.py
+++ artauction/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('webapp/', include('webapp.urls')),
-    path('admin/', admin.site.urls),
+    path("webapp/", include("webapp.urls")),
+    path("admin/", admin.site.urls),
 ]
